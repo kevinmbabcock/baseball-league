8d6ab8b5fac7e661aa1b174c9590e0c8dd16c4ea
Guest User <Guest@epicodus-7.local>
2018-01-29 13:00:14 -0800

    add schedule component to display game objects

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -6,12 +6,18 @@ import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/databa
 @Injectable()
 export class PlayerService {
   players: FirebaseListObservable<any[]>;
+  games: FirebaseListObservable<any[]>;
 
   constructor(private database: AngularFireDatabase) {
      this.players = database.list('players');
+     this.games = database.list('games');
   }
 
   getPlayerById(playerId: string) {
     return this.database.object('players/' + playerId);
+  }
+
+  getGames() {
+    return this.games;
   }
 }
