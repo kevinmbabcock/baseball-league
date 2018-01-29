374f99ae4a49a54be4c12a891fe54667dcbb3ec0
Kevin Babcock <kevinmbabcock@icloud.com>
2018-01-29 13:11:14 -0800

    add navbar link and route to admin component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -6,6 +6,7 @@ import { RosterComponent } from './roster/roster.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { TeamDetailsComponent } from './team-details/team-details.component';
 import { PlayerDetailsComponent } from './player-details/player-details.component';
+import { AdminComponent } from './admin/admin.component';
 
 
 
@@ -33,6 +34,10 @@ const appRoutes: Routes = [
   {
     path: 'players/:id',
     component: PlayerDetailsComponent
+  },
+  {
+    path: 'admin',
+    component: AdminComponent
   }
 ];
 
