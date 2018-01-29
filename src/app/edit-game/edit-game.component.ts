d548a4b1e1c067531f238f6bf6881ecc2b999401
Guest User <Guest@epicodus-7.local>
2018-01-29 14:41:36 -0800

    add edit game component to update and delete from schedule

--- src/app/edit-game/edit-game.component.ts
+++ src/app/edit-game/edit-game.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from '../player.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-edit-game',
   templateUrl: './edit-game.component.html',
-  styleUrls: ['./edit-game.component.css']
+  styleUrls: ['./edit-game.component.css'],
+  providers: [PlayerService]
 })
 export class EditGameComponent implements OnInit {
+  games: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
+    this.games = this.playerService.getGames();
+  }
+
+  beginUpdatingGame(gameToUpdate){
+    this.playerService.updateGame(gameToUpdate);
+  }
+
+  beginDeletingGame(gameToDelete) {
+    if(confirm("Are you sure you want to delete this item from the inventory?")){
+      this.playerService.deleteGame(gameToDelete);
+    }
   }
 
 }
