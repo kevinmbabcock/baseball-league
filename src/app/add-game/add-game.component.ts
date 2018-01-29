521e743852fb2b97c38fba9d1db2035e9456f9d4
Guest User <Guest@epicodus-7.local>
2018-01-29 13:51:09 -0800

    add add game component

--- src/app/add-game/add-game.component.ts
+++ src/app/add-game/add-game.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Game } from '../game.model';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-add-game',
   templateUrl: './add-game.component.html',
-  styleUrls: ['./add-game.component.css']
+  styleUrls: ['./add-game.component.css'],
+  providers: [PlayerService]
 })
 export class AddGameComponent implements OnInit {
 
-  constructor() { }
+  constructor(private playerService: PlayerService) { }
 
   ngOnInit() {
+  }
+
+  submitForm(team1: string, team2: string, date: string, field: string) {
+    const newGame: Game = new Game(team1, team2, date, field);
+    this.playerService.addGame(newGame);
   }
 
 }
--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Player } from './player.model';
+import { Game } from './game.model';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 
@@ -24,5 +25,24 @@ export class PlayerService {
   getPlayers() {
     return this.players;
 
+  }
+
+  addGame(newGame: Game) {
+    this.games.push(newGame);
+  }
+
+  getGameById(gameId: string){
+    return this.database.object('games/' + gameId);
+  }
+
+  updateGame(localUpdatedGame){
+    const gameEntryInFirebase = this.getGameById(localUpdatedGame.$key);
+    gameEntryInFirebase.update({team1: localUpdatedGame.team1,
+    team2: localUpdatedGame.team2, date: localUpdatedGame.date, field: localUpdatedGame.field});
+  }
+
+  deleteGame(localGameToDelete){
+    const gameEntryInFirebase = this.getGameById(localGameToDelete.$key);
+    gameEntryInFirebase.remove();
   }
 }
