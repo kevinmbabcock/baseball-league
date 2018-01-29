24e724ea88cadb5d57e4bbefb7e803403b2334ff
Guest User <Guest@epicodus-7.local>
2018-01-29 11:15:19 -0800

    add getPlayerById method to service

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Player } from './player.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 @Injectable()
 export class PlayerService {
+  playerss: FirebaseListObservable<any[]>;
 
-  constructor() { }
-
+  constructor(private database: AngularFireDatabase) {
+     this.players = database.list('players');
+  }
+  getPlayerById(playerId: string) {
+    return this.database.object('players/' + playerId);
+  }
 }
