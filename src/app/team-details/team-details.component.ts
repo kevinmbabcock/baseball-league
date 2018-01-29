0cf2ea912ca556179b490dd7839dcae85460fbb9
Guest User <Guest@epicodus-94.local>
2018-01-29 10:45:52 -0800

    add content to player-details.html and team-details.html

--- src/app/team-details/team-details.component.ts
+++ src/app/team-details/team-details.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { FirebaseObjectObservable } from 'angularfire2/database';
+import { Player } from '../player.model';
+import { PlayerService } from '../player.service';
 
 @Component({
   selector: 'app-team-details',
   templateUrl: './team-details.component.html',
-  styleUrls: ['./team-details.component.css']
+  styleUrls: ['./team-details.component.css'],
+  providers: [PlayerService]
 })
 export class TeamDetailsComponent implements OnInit {
+  teamName: string;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private location: Location, private playerService: PlayerService) { }
 
   ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.teamToDisplay = urlParameters['id'];
+    });
   }
 
 }
