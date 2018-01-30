8751e59ee2bffefdf63201d9040069195e183eb8
Guest User <Guest@epicodus-94.local>
2018-01-30 15:35:30 -0800

    add drop down menu to add-player component

--- src/app/add-player/add-player.component.ts
+++ src/app/add-player/add-player.component.ts
@@ -1,18 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { PlayerService } from '../player.service';
 import { Player } from '../player.model';
+import { Team } from '../team.model';
+import { TeamService } from '../team.service';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-add-player',
   templateUrl: './add-player.component.html',
   styleUrls: ['./add-player.component.css'],
-  providers: [PlayerService]
+  providers: [PlayerService, TeamService]
 })
 export class AddPlayerComponent implements OnInit {
+  positions: string[] = ["P", "C", "1B", "2B", "3B", "SS", "RF", "CF", "LF", "RF", "INF", "OF", "UTIL"];
+  teams: FirebaseListObservable<any[]>;
 
-  constructor(private playerService: PlayerService) { }
+  constructor(private playerService: PlayerService, private teamService: TeamService) { }
 
   ngOnInit() {
+    this.teams = this.teamService.getTeams();
+    console.log(this.positions);
+    console.log(this.teams);
   }
 
   submitForm(name: string, age: string, position: string, team: string, jerseyNumber: string) {
