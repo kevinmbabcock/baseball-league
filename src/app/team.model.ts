230096d89c8911bdc827696fb9a9f23b1f7b0efb
Guest User <Guest@epicodus-7.local>
2018-01-30 13:39:40 -0800

    fix typo in html in roster component

--- src/app/team.model.ts
+++ src/app/team.model.ts
@@ -3,10 +3,10 @@ import { Game } from './game.model';
 
 export class Team {
 
-  public players: Player = [];
-  public games: Game = [];
-  public wins: 0;
-  public losses: 0;
-  constructor (public name: string, public manager: string) { }
+  // public players: Player = [];
+  // public games: Game = [];
+  // public wins: 0;
+  // public losses: 0;
+  // constructor (public name: string, public manager: string) { }
 
 }
