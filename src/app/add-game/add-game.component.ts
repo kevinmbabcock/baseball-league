9ebae6025d18ff328b3c5b2d3a761902e838175e
Malgorzata Haniszewska <gosia.haniszewska@gmail.com>
2018-01-30 22:05:31 -0800

    add dropdown menu to add new game

--- src/app/add-game/add-game.component.ts
+++ src/app/add-game/add-game.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { Game } from '../game.model';
 import { GameService } from '../game.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { TeamService } from '../team.service';
 
 @Component({
   selector: 'app-add-game',
   templateUrl: './add-game.component.html',
   styleUrls: ['./add-game.component.css'],
-  providers: [GameService]
+  providers: [GameService, TeamService]
 })
 export class AddGameComponent implements OnInit {
-
-  constructor(private gameService: GameService) { }
+  teams: FirebaseListObservable<any[]>;
+  constructor(private gameService: GameService, private teamService: TeamService) { }
 
   ngOnInit() {
+    this.teams = this.teamService.getTeams();
   }
 
   submitForm(team1: string, team1Score: string, team2: string, team2Score: string, date: string, field: string) {
