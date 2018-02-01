09d247f6ce6565a55169d5a6ca69565dd74c7154
Guest User <Guest@epicodus-94.local>
2018-02-01 09:05:48 -0800

    add pipe to master schedule to allow filtering by team

--- src/app/schedule/schedule.component.ts
+++ src/app/schedule/schedule.component.ts
@@ -1,21 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { GameService } from '../game.service';
 import { FirebaseListObservable } from 'angularfire2/database';
+import { TeamService } from '../team.service';
 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
   styleUrls: ['./schedule.component.css'],
-  providers: [GameService]
+  providers: [GameService, TeamService]
 })
 export class ScheduleComponent implements OnInit {
   @Input('teamName') filterByTeamName: string;
   games: FirebaseListObservable<any[]>;
+  teams: FirebaseListObservable<any[]>;
+  filterByTeam: string = "";
 
-  constructor(private gameService: GameService) { }
+  constructor(private gameService: GameService, private teamService: TeamService) { }
 
   ngOnInit() {
     this.games = this.gameService.getGames();
+    this.teams = this.teamService.getTeams();
+  }
+
+  onChange(optionFromMenu) {
+    this.filterByTeam = optionFromMenu;
   }
 
 }
