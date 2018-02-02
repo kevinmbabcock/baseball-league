9a00098275c4733ab11b5e8724c02142bdce3624
Guest User <Guest@epicodus-7.local>
2018-02-01 17:16:29 -0800

    refactor game pipe

--- src/app/game.pipe.ts
+++ src/app/game.pipe.ts
@@ -10,51 +10,24 @@ export class GamePipe implements PipeTransform {
   transform(input: Game[], desiredGameTeam) {
     if (input) {
       const output: Game[] = [];
-      if (desiredGameTeam === "Titans") {
-        for (let i=0; i < input.length; i++) {
-          if ((input[i].team1 === "Titans") || (input[i].team2 === "Titans")) {
-            output.push(input[i]);
-          }
-        }
-        return output;
-      } else if (desiredGameTeam === "Tigers") {
-        for (let i=0; i < input.length; i++) {
-          if ((input[i].team1 === "Tigers") || (input[i].team2 === "Tigers")) {
-            output.push(input[i]);
-          }
-        }
-        return output;
-      } else if (desiredGameTeam === "Rage") {
-        for (let i=0; i < input.length; i++) {
-          if ((input[i].team1 === "Rage") || (input[i].team2 === "Rage")) {
-            output.push(input[i]);
-          }
-        }
-        return output;
-      } else if (desiredGameTeam === "Expos") {
-        for (let i=0; i < input.length; i++) {
-          if ((input[i].team1 === "Expos") || (input[i].team2 === "Expos")) {
-            output.push(input[i]);
-          }
-        }
-       return output;
-     } else if (desiredGameTeam === "Brewers") {
-        for (let i=0; i < input.length; i++) {
-          if ((input[i].team1 === "Brewers") || (input[i].team2 === "Brewers")) {
-            output.push(input[i]);
-          }
-        }
-        return output;
-      } else if (desiredGameTeam === "Braves") {
-         for (let i=0; i < input.length; i++) {
-           if ((input[i].team1 === "Braves") || (input[i].team2 === "Braves")) {
-             output.push(input[i]);
-           }
-         }
-         return output;
-      } else {
+      let teamNames: string[] = ["Titans", "Tigers", "Rage", "Expos", "Brewers", "Braves"];
+      console.log(desiredGameTeam);
+      if (!(desiredGameTeam) || (desiredGameTeam === "All Teams")){
         return input;
       }
+      teamNames.forEach(function(teamName) {
+        if (desiredGameTeam === teamName) {
+          for (let i=0; i < input.length; i++) {
+            if ((input[i].team1 === teamName) || (input[i].team2 === teamName)) {
+              output.push(input[i]);
+            }
+          }
+        }
+      })
+      return output;
+    } else {
+    return input;
+
     }
   }
 
